85164cf6247166130c958ac235a1c57aec3b1c43
bits-32 <bits_16@outlook.com>
2026-09-23 18:13:46 +0200

    Update and rename vjezba_2.cbp to vjezba_2.cpp

---
+++ vjezba_2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    //string a = "sedam";
+    // char a[10] = "sedam";
+
+    // cout << a[10] << endl;
+
+//    int n;
+//    cin >> n;
+//
+//    if (n < 5)
+//    {
+//        cout << "Broj je manji od 5";
+//
+//    } else if (n == 5)
+//    {
+//        cout << "Broj je jednak 5";
+//
+//    } else if ((n > 5) && (n < 100))
+//    {
+//
+//        cout << "Broje je veci od 5";
+//
+//    } else {
+//        cout << "Broj je velik";
+//
+//    }
+
+
+//    string a[] = {"jedan", "dva","tri"};
+//
+//    //cout << a[4];
+//
+//    for (int i = 0; i < 3; i++)
+//    {
+//        cout << a[i] << endl;
+//
+
+//    }
+//    string model[] = {"G26", "X1 SUV", "X3 SUV", "Model S", "Model Y", "Model 3", "BENZ C-Class", "Eletric GLB", "EQA", "CX-6e", "CX-80", "CX-30", "296", "Luce", "CZ26"};
+//    string brand[] = {"BMW", "Tesla", "Mercedes", "Mazda", "Ferrari"};
+//
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cout << "Brand: " << brand[i] << ", Modeli: ";
+//        for (int j = 0; j < 3; j++)
+//        {
+//            cout << model[(i * 3) + j] << ", ";
+//
+//        }
+//
+//        cout << endl;
+//
+//    }
+
+
+
+    return 0;
+}
